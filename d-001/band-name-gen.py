ae139d668a4268c3cde1d554d753c78ef98a461f
apitofme <apitofme@github.com>
2024-04-20 02:49:36 +0100

    d-001: fixed a typo

--- d-001/band-name-gen.py
+++ d-001/band-name-gen.py
@@ -4,5 +4,5 @@
 """
 print("Welcome to the Band Name Generator.")
 city = str(input("What's the name of the city you grew up in?\n"))
-pet = str(input("What's your pet's name?!\n"))
+pet = str(input("What's your pet's name?\n"))
 print(f"Your band name could be {city} {pet}")
