7772cae21907a7cd6a716b76c954487a66834ec5
apitofme <apitofme@github.com>
2024-04-20 03:07:46 +0100

    d-001: cleaned up code by removing unnecessary type-casting on input

--- d-001/band-name-gen.py
+++ d-001/band-name-gen.py
@@ -3,6 +3,6 @@
 - Day 001:  Band Name Generator
 """
 print("Welcome to the Band Name Generator.")
-city = str(input("What's the name of the city you grew up in?\n"))
-pet = str(input("What's your pet's name?\n"))
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
 print(f"Your band name could be {city} {pet}")
