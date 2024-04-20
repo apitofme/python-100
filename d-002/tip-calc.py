3fd54789b551eff23aefdb4ac449d78ef2ff966a
apitofme <apitofme@github.com>
2024-04-20 02:52:03 +0100

    d-002: initial solo attempt of Day 2 project before following lesson videos

---
+++ d-002/tip-calc.py
@@ -0,0 +1,10 @@
+"""
+-=< 100 Days of Python >=-
+- Day 002:  Tip Calculator
+"""
+print("Welcome to the Tip Calculator!")
+bill = float(input("What was the total bill? $"))
+tip = float(input("How much tip would you like to give? 10, 12 or 15? "))
+people = int(input("How many people to split the bill? "))
+pay = (bill / people) * (tip / 100 + 1)
+print(f"Each person should pay: ${pay:.2f}")
