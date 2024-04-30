a5845ef656e053206a2e3426423a16b06ddf1c18
apitofme <apitofme@github.com>
2024-04-30 10:26:50 +0100

    d-002: Updated notes and amended for consistant formatting

--- d-002/data_types.py
+++ d-002/data_types.py
@@ -9,7 +9,7 @@ L.19 - Primitive Data Types:
 As we've seen before we can print a string using the 'print()' function:
 """
 print("Hello")
-# We can use either single or double quotes, as long as we're consistent.
+# We can use either single or double quotes, as long as we're consistent!
 print('World')
 
 """
@@ -23,10 +23,10 @@ world = "World"
 print(world[1]) # -> 'o'
 
 """
-It is also possible to count backwards from the end of the string,
-however since zero points to the first letter on the left then the
-last character (i.e. furthest right) starts at index '-1', then '-2'
-and so on moving from right to left back through the string.
+It is also possible to count backwards from the end of the string!
+However since zero points to the first letter on the left then the last
+character (i.e. furthest right) starts at index '-1', then '-2' and so on
+moving from right to left back through the string.
 """
 # So the 'o' in "Hello" can be accessed at either index '4' or '-1':
 print("Hello"[4])
@@ -35,7 +35,7 @@ print("Hello"[-1])
 """
 >> Integer: a whole number.
 Python recognises raw numbers as such and will perform operations
-accordingly
+accordingly:
 """
 print(123 + 345) # -> 468
 """
@@ -46,8 +46,8 @@ print("123" + '345') # -> "123456" (i.e. string concatenation)
 
 """
 Note: We typically use commas to make a large number more readable
-(e.g. 123,456), in Python we can actually do the same thing only
-we use underscores '_' instead of commas.
+(e.g. 123,456), in Python we can actually do the same thing only we use
+underscores '_' instead of commas.
 """
 print(123_456_789)
 
@@ -55,11 +55,11 @@ print(123_456_789)
 >> Float: a 'floating-point' number
 -- i.e. a decimal representation of a fractional or 'non-whole' number.
 """
-3.14159
+pi = 3.14159 # Aside: 'pi' is also an "irrational" number! ;)
 
 """
->> Boolean: "True" or "False"
--- often considered equivalent to '1' or '0' respectively (blame binary!).
+>> Boolean: literally "True" or "False" values
+-- equivalent to '1' or '0' respectively (blame binary!).
 """
 True
 False
@@ -89,16 +89,18 @@ So, since the string part has no numerical meaning and the integer (at
 least as far as Python is concerned) has no textual meaning, it throws an
 error.
 
--- Note: the 'len()' function returns a TypeError if we pass in a number!
+-- Note: the 'len()' function also returns a TypeError if given a number!
 
 Anyway, in order to fix this error we can use another function in Python
-to convert (or 'cast') the variable's data-type to a string:
+to convert the variable's data-type to a string:
 """
 str_length = str(length)
 print(type(str_length)) # -> "str" (string)
 print(f"Your name has " + str_length + " characters")
 
-# There are of course functions to convert to other data-types too:
+"""
+There are of course functions to convert to other data-types too:
+"""
 int_number = int("123") # converts the string "123" to an integer
 print(type(int_number))
 
@@ -106,21 +108,28 @@ float_number = float("3.14") # converts the string to a floating-point
 print(type(float_number))
 
 """
-It is also possible to cast/convert to a Boolean value using the 'bool()'
+It is also possible to convert to a Boolean value using the 'bool()'
 function. Remember earler we said that Boolean values (i.e. True / False)
 can also be represented as '1' or '0'
 """
 print(bool(1)) # -> "True" - converts the integer '1' to a Boolean
 print(bool(0)) # -> "False"
 
-# However there are some quirks to consider when casting to a 'bool':
-# - any number that is NOT zero equates to "True"
+# However there are some quirks to consider when converting to a 'bool':
+# - any number that is NOT zero equates to "True" (even negative numbers)!
+print(bool(2)) # -> "True"
 print(bool(-1)) # -> "True"
 
 # - anything else that is considered to have a value is also "True",
 #   this includes a string of the number zero
 print(bool("0")) # -> "True"
 print(bool(3.14)) # -> "True"
+
 # - an empty string however DOES equate to "False"
 print(bool("")) # -> "False"
 # ...As does any other EMPTY data-structure, but we'll get to those later!
+
+"""
+L.21 - Coding Exercise: Data Types
+"""
+# See external file: "exercise_data_types.py"
