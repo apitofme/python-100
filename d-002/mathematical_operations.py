f18ccefe6fedf349606b4766158f7eafef4aefd3
apitofme <apitofme@github.com>
2024-05-01 10:23:18 +0100

    d-002: Updated lesson notes

--- d-002/mathematical_operations.py
+++ d-002/mathematical_operations.py
@@ -5,34 +5,31 @@
 """
 L22 - Simple Math Operators:
 """
-# Add [plus sign]
-3 + 5
+# Addition, which uses the plus sign.
+3 + 5 # => 8
 
-# Subtract [dash or 'minus' sign]
-7 - 4
+# Subtraction, which uses the 'minus' sign (a.k.a hyphen / dash).
+7 - 4 # => 3
 
-# Multiply [star or 'asterix']
-3 * 2
+# Multiplication, uses the asterix or 'star' symbol.
+3 * 2 # => 6
 
-# Divide [forward slash]
-6 / 3
-# = 2.0
+# Division, using the forward-slash symbol.
+6 / 3 # => 2.0
 """
-Note: Division will always return a float!
-Even if it divides cleanly in to a whole number.
+Note: Division will always return a float, even if it divides cleanly!
 
-Python can mix integer and floating point numbers in operations,
-but the result will always be a float.
+Also, Python can mix integer and floating point numbers in operations,
+but again the result will always be a float.
 """
-print(2 + 3.0)
-# = 5.0
+print(2 + 3.0) # => 5.0
 
 """
-Python has other simple operators too, such as
-the double-asterix for raising a number to a power:
-e.g. 2 "to the power of" 3 = (2 x 2) x 2 = 8
+Python has other useful math operators too, such as the double-asterix
+for raising a number to a power:
+-- e.g. 2 "to the power of" 3 [ i.e. (2 x 2) x 2 = 8 ]
 """
-2 ** 3
+2 ** 3 # => 8
 
 """
 Order of Operations: (i.e. "BODMAS" // "PEMDAS")
@@ -48,22 +45,27 @@ This is becuase they are acutally considered EQUAL in priority,
 as are 'A' and 'S'. So in either case the priority becomes simply
 the order they are written.
 """
-print(3 * 3 + 3 / 3 - 3)
-# = 7.0
+print(3 * 3 + 3 / 3 - 3) # => 7.0
 
-# Challenge: modify the above to get the answer '3'
+# Challenge: modify the above to get the answer '3'.
 print(3 * (3 + 3) / 3 - 3)
 """
 By placing brackets around '3 + 3' we make that the highest prioriy,
 so we can demonstrate the process of the equation as follows:
 
--       3 * (3 + 3) / 3 - 3
-->      3 * (6) / 3 - 3     -- multiplication is next (leftmost)
--->     18 / 3 - 3          -- after that is division
---->    6.0 - 3               -- and lastly, subtraction
----->   = 3.0
+>       3 * (3 + 3) / 3 - 3     -- we start with what's inside the '( )'
+->      3 * (6) / 3 - 3         -- multiplication is next (leftmost) so...
+-->     18 / 3 - 3              -- after that is division...
+--->    6.0 - 3                 -- and lastly the subtraction...
+---->   = 3.0                   -- giving us the answer '3'
 
-Remember that division *always* gives a floating point number,
-so even though we're dealing with whole numbers we got "6.0"
-from the division of "18 / 3", after which we retain the float.
+Remember that division always results in a floating point number, so even
+though we're dealing with whole numbers we actually get "6.0" from the
+division of "18 / 3", after which we retain the float through all
+subsequent operations!
 """
+
+"""
+L.23 - Coding Exercise: BMI Calculator
+"""
+# See external file: "exercise_bmi_calculator.py"
--- d-002/number_manipulation.py
+++ d-002/number_manipulation.py
@@ -9,12 +9,12 @@ L.24 - Number Manipulation & F-Strings:
 The 'round()' Function:
 Ref: https://docs.python.org/3/library/functions.html#round
 
-This built-in function in Python allows us round a floating point number...
+This built-in function in Python allows us round a floating point number:
 """
 # - to a whole number
-print(round(8 / 3)) # -> 3
-# - OR - to a specified number of deciaml places (e.g. 2)
-print(round(8 / 3, 2)) # -> 2.67
+print(round(8 / 3)) # => 3
+# - or to a specified number of deciaml places (e.g. 2)
+print(round(8 / 3, 2)) # => 2.67
 """
 As you can see the second parameter is optional, and is used to specify
 decimal precision (i.e. how many digits we want after the decimal point)
@@ -36,7 +36,7 @@ fractional part (if any) that the normal division would produce.
 So the result of a floor division is just the whole number part
 -- i.e. the bit before the decimal point
 """
-print(8 // 3) # -> 2
+print(8 // 3) # => 2
 # Notice how the result is different from what we got using 'round()'
 """
 Also, remember how division in Python always returns a floating point
@@ -46,7 +46,7 @@ fractional component)?
 Well since floor division doesn't produce a fractional component it will
 always return an integer ('int')!
 """
-print(type(8 // 3))
+print(type(8 // 3)) # => 'int'
 
 """
 Combination Assignment Operators:
@@ -54,12 +54,15 @@ Combination Assignment Operators:
 Previously we have 'assigned' an input value (or other piece of data to
 work with) to a variable name with the equals sign -- e.g. variable = data
 
-However Python has a kind of short-hand notation which allows us combine
-this assignment with a basic math operation, as follows:
+However Python also has a short-hand notation which allows us combine an
+assignment with a basic math operation, as follows:
 """
-# First we assign a variable name a value using the equals sign
+# First we assign a value to a variable using the equals sign
 num = 0
-
+"""
+Now we can perform an operation whilst re-assigning the new value back
+on to the variable using an assignment operator:
+"""
 # Addition -- equivalent to: num = num + 2
 num += 4
 print(num)
@@ -75,22 +78,40 @@ print(num)
 # Division -- equivalent to: num = num / 6
 num /= 6
 print(num)
-
+"""
+This offers a convenient short-hand way to update the value stored on a
+variable 'in-place' (i.e. overwrite the previous value). Particularly for
+any kind of incremental counter or other variable that is updated on each
+cycle through a loop.
+"""
 
 """
-F-Strings:
+F-Strings: -- i.e. "Formatted String Literals"
+Ref: https://docs.python.org/3/reference/lexical_analysis.html#f-strings
 
-These allow us to use different data-types in a string without the need to
-convert them first (i.e. with 'str()'), and to use them all we need to do
-is to type an 'f' before we start to declare a string, i.e:
+An f-string allows us to use different data-types in a string without the
+need to convert them first [e.g. with 'str()']. In Python we define an
+f-string by simply typing an 'f' before the opening qutation-mark when
+declaring a string -- i.e.
 
     "This is a string"
     f"This is an f-string!"
 
-Then whenever we want to use a stored variable in an f-string we simply
-wrap the variable name in a pair of curly-braces '{}'
+Any data or expression we want to be interpreted inside of the f-string
+must be wrapped in a pair of curly-braces '{}' within the string,
+which is then evaluated at run time.
+
+For example: Given 3 different data types...
 """
-score = 5
-height = 1.8
-is_winning = True
-print(f"Your score is {score}; Your height is {height}; and it is {is_winning} that you are winning!")
+score = 5               # an 'int'
+height = 1.8            # a 'float'
+is_winning = True       # a 'bool'
+
+# We can print them all out using an f-string (without converting them)
+print(f"Your score is {score}; Your height is {height}; and it is \
+    {is_winning} that you are winning!")
+
+"""
+L.25 - Coding Exercise: Life in Weeks
+"""
+# See external file: "exercise_life_in_weeks.py"
