88a17dc07f839daf880a25d0b240a3ea6e4c0352
apitofme <apitofme@github.com>
2024-05-02 10:28:51 +0100

    d-002: Added notes for the final two coding exercises

---
+++ d-002/exercise_bmi_calculator.py
@@ -0,0 +1,59 @@
+"""
+-=< 100 Days of Python >=-
+-=[ Day 002 ]=-
+
+Coding Exercise: BMI Calculator (L.23)
+"""
+"""
+Slide 1: Task - write a program that calculcates a person's BMI
+
+BMI stand for "Body Mass Index" and it is used as a measure of a person's
+weight relative to their height to determine what is considered "healthy"
+for a given individual (though the method has it's flaws and critics).
+
+The formula used to calculate BMI is given as:
+
+                weight (kg)
+    BMI =   -------------------
+            height (m) squared      -- i.e. height x height (in meters)
+
+So, with inputs given as "height (in meters)" and "weight (in kilograms)"
+see if you can calculate the BMI and print it out as an integer.
+"""
+height = input()
+weight = input()
+
+"""
+Slide 2: Solution
+"""
+# First we want to convert the inputs, which we recieve as strings,
+# in to numbers that we can perform mathematical operations on...
+print(type(height)) # we can check the type of input value ('str')
+weight_as_int = int(weight)
+height_as_float = float(height)
+# Using the exponent operator **
+bmi = weight_as_int / height_as_float ** 2
+# or using multiplication and BODMAS (a.k.a. PEMDAS)
+bmi = weight_as_int / (height_as_float * height_as_float)
+
+bmi_as_int = int(bmi)
+print(bmi_as_int)
+
+"""
+My Solution:
+"""
+# First convert the input strings to numerical values
+height = float(height)
+weight = float(weight)
+
+# Then calculate the BMI (remembering BODMAS)
+bmi = weight / height ** 2
+# -- OR -- bmi = weight / (height * height)
+
+# Finally, print out the BMI value as an integer
+print(int(bmi))
+
+"""
+Ref:
+https://app.auditorium.ai/lesson/eelyNMYJKXeNJAbjssSEQz0m88XvnhX6/3c6091ed-ef4e-4247-82ae-167a2cb5208f?sl=1ea8e61d-4da3-4d1b-9c45-aed9ac32a221&st=Lkstz5KiPP91iISobr9xtB8AeKba3EVt
+"""
---
+++ d-002/exercise_life_in_weeks.py
@@ -0,0 +1,30 @@
+"""
+-=< 100 Days of Python >=-
+-=[ Day 002 ]=-
+
+Coding Exercise: Life in Weeks (L.25)
+"""
+"""
+Slide 1: Task
+- Create a program that calculates how many weeks we have left
+to live (assuming we will live to be 90).
+
+The input will be a person's current age, and the output should
+be a message in the following format: "You have x weeks left."
+"""
+age = input()
+
+"""
+Slide 2: Solution
+"""
+# First take the current age from the total (assumed) years
+years_left = 90 - int(age)
+# Then convert that number of years in to weeks
+weeks_left = years_left * 52
+# Finaly, print the output using an f-string
+print(f"You have {weeks_left} weeks left.")
+
+"""
+Ref:
+https://app.auditorium.ai/lesson/eelyNMYJKXeNJAbjssSEQz0m88XvnhX6/70ce34ac-2968-4f2d-a523-964c824f7892?sl=42bdaa61-096d-4f32-8f1c-371f681345f6&st=voOVSS863JuNoRgjspfGFZBADWEmqWar
+"""
