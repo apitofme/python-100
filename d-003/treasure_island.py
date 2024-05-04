f37d221901fbbde6b214063e02a9d5b09e626a9f
apitofme <apitofme@github.com>
2024-05-04 12:34:13 +0100

    d-003: Applied snake_case to filename + Updated formatting for consistency

--- d-003/treasure_island.py
+++ d-003/treasure_island.py
@@ -1,6 +1,8 @@
 """
 -=< 100 Days of Python >=-
-- Day 003:  Treasure Island
+-=[ Day 003 ]=-
+
+Project: Treasure Island
 """
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
@@ -9,7 +11,7 @@ print("Your mission is to find the treasure.")
 print("You're at a cross-road. Where do you want to go?")
 choice = input('Type "left" or "right":\n').lower()
 # validate input
-while choice not in ['left','right']:
+while choice not in ['left', 'right']:
     print("Sorry, I didn't understand that.")
     choice = input('Please type "left" or "right":\n').lower()
 # process input
@@ -19,11 +21,13 @@ if choice == 'right':
 
 # Lake crossing:
 print("You've come to a lake. There is an island in the middle of the lake.")
-choice = input('Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
+choice = input(
+    'Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
 # validate input
-while choice not in ['wait','swim']:
+while choice not in ['wait', 'swim']:
     print("Sorry, I didn't understand that.")
-    choice = input('Please type "wait" to wait for a boat, or "swim" to swim across.\n').lower()
+    choice = input(
+        'Please type "wait" to wait for a boat, or "swim" to swim across.\n').lower()
 # process input
 if choice == 'swim':
     print("You get attacked by an angry trout. Game Over")
@@ -31,10 +35,12 @@ if choice == 'swim':
 
 # Coloured doors:
 print("You arrive at the island unharmed. There is a house with 3 doors.")
-choice = input('One red, one yellow and one blue. Which colour do you choose?\n').lower()
-while choice not in ['red','yellow','blue']:
+choice = input(
+    'One red, one yellow and one blue. Which colour do you choose?\n').lower()
+while choice not in ['red', 'yellow', 'blue']:
     print("Sorry, I didn't understand that.")
-    choice = input('Which colour do you choose? [Please type "red", "yellow" or "blue"]\n').lower()
+    choice = input(
+        'Which colour do you choose? [Please type "red", "yellow" or "blue"]\n').lower()
 # process input
 if choice == 'red':
     print("It's a room full or fire. Game Over")
