96cba810aab297515c8cb0e9125707bb6aad8718
apitofme <apitofme@github.com>
2024-05-04 12:32:00 +0100

    d-003: BONUS exercise from on-going example in Day 3's lessons

--- d-003/rollercoaster_midlife_crisis.py
+++ d-003/rollercoaster_midlife_crisis.py
@@ -1,3 +1,21 @@
+"""
+-=< 100 Days of Python >=-
+-=[ Day 003 ]=-
+
+Bonus Exercise: Rollercoaster Ticket (Midlife-Crisis Edition)
+"""
+"""
+Premise:
+Building on the running example from Day 3's lessons - the idea of a
+rollercoaster ticketing office, where tickets can only be sold to people
+over 120cm tall, and cost different amounts based on age.
+
+We updated it once already to implement the option to pay an additional
+amount for a photo.
+
+Now we want to update the program to allow people going through their
+"midlife crisis" to ride for free!
+"""
 height = int(input("How tall are you (in cm)?\n"))
 if height >= 120:
     bill = 0
@@ -11,18 +29,19 @@ if height >= 120:
         # Youth ticket (between 12 and 18)
         print("A youth ticket is $8")
         bill += 8
-    if age >= 45 and age <= 55:
+    elif age >= 45 and age <= 55:
         # Mid-life Crisis ticket (i.e. Free)
         print("You're having a mid-life crisis, you ride for FREE!")
     else:
         # Adult ticket (over 18)
         print("An adult ticket is $12")
         bill += 12
-    
+
     photo = input("Do you want a photo? Type 'Y' or 'N'\n").upper()
     if photo == 'Y':
         bill += 3
-        
+
     print(f"Your total ticket price is: ${bill}")
+
 else:
     print("Sorry, you're too small to ride.")
