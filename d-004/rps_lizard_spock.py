e9125b738dd9b307caf9cdd4e5a4791305c8ea21
apitofme <apitofme@github.com>
2024-05-11 12:57:32 +0100

    d-004: Added context to win / lose messages in RPSLS (e.g. "Lizard poisons Spock")

--- d-004/rps_lizard_spock.py
+++ d-004/rps_lizard_spock.py
@@ -127,34 +127,83 @@ print(game[computer_choice])
 
 # win / lose messages
 win = "Congratulations, You Win!"
-lose = "Sorry, You Lost!"
+lose = "Sorry, You Lose!"
 
 # determine winner
 if player_choice == computer_choice:
     print("It's a draw!")
 else:
     if computer_choice == "Rock":
-        if player_choice in ["Paper", "Spock"]:
+
+        if player_choice == "Paper":
+            print("Paper covers Rock.")
             print(win)
-        else:
+        elif player_choice == "Spock":
+            print("Spock vaporises Rock.")
+            print(win)
+        elif player_choice == "Scissors":
+            print("Rock breaks Scissors.")
             print(lose)
+        else:
+            print("Rock crushes Lizard.")
+            print(lose)
+
     elif computer_choice == "Paper":
-        if player_choice in ["Scissors", "Lizard"]:
+
+        if player_choice == "Scissors":
+            print("Scissors cut Paper.")
             print(win)
-        else:
+        elif player_choice == "Lizard":
+            print("Lizard eats Paper.")
+            print(win)
+        elif player_choice == "Rock":
+            print("Paper covers Rock.")
             print(lose)
+        else:
+            print("Paper disproves Spock.")
+            print(lose)
+
     elif computer_choice == "Scissors":
-        if player_choice in ["Rock", "Spock"]:
+
+        if player_choice == "Rock":
+            print("Rock breaks Scissors.")
             print(win)
-        else:
+        elif player_choice == "Spock":
+            print("Spock smashes Scissors.")
+            print(win)
+        elif player_choice == "Paper":
+            print("Scissors cut Paper.")
             print(lose)
+        else:
+            print("Scissors decapitate Lizard.")
+            print(lose)
+
     elif computer_choice == "Lizard":
-        if player_choice in ["Rock", "Scissors"]:
+
+        if player_choice == "Rock":
+            print("Rock crushes Lizard.")
             print(win)
-        else:
+        elif player_choice == "Scissors":
+            print("Scissors decapitate Lizard.")
+            print(win)
+        elif player_choice == "Paper":
+            print("Lizard eats Paper.")
             print(lose)
-    elif computer_choice == "Spock":
-        if player_choice in ["Paper", "Lizard"]:
-            print(win)
         else:
+            print("Lizard poisons Spock.")
+            print(lose)
+
+    elif computer_choice == "Spock":
+
+        if player_choice == "Paper":
+            print("Paper disproves Spock.")
+            print(win)
+        elif player_choice == "Lizard":
+            print("Lizard poisons Spock.")
+            print(win)
+        elif player_choice == "Rock":
+            print("Spock vaporises Rock.")
+            print(lose)
+        else:
+            print("Spock smashes Scissors")
             print(lose)
