cd3f0a70e9c3fb73fcdd96645a9e5e9648c44dea
apitofme <apitofme@github.com>
2024-05-28 12:23:55 +0100

    d-008: Project - part 2 done

--- d-008/caesar_cipher.py
+++ d-008/caesar_cipher.py
@@ -24,7 +24,7 @@ shift = int(input("Type the cipher's shift offset: "))
 
 
 def encrypt(plain_text, shift_amount):
-    """use list indexing to implement a 'Caesar' cipher encryption"""
+    """encrypt plain-text using "Caesar's Cipher" by adding the shift"""
     cipher_length = len(alphabet)
     encoded_text = ""
     for char in plain_text:
@@ -32,13 +32,44 @@ def encrypt(plain_text, shift_amount):
         if char not in alphabet:
             encoded_text += char
         else:
-            # calculate the cipher encoded character's look-up reference
+            # calculate the cipher-encoded reference for the character
             cipher_index = alphabet.index(char) + shift_amount
-            # better than using hard-coded "26" >> allows flexibility
-            # NOTE: list index starts at 0 (zero)! [hence the "-1"]
+            # keep the look-up reference within the cipher's range
+            # NOTE: better than using hard-coded "26" >> allows flexibility
+            #       + list index starts at 0 (zero)! [hence the "-1"]
             if cipher_index > cipher_length - 1:
                 cipher_index -= cipher_length
 
             encoded_text += alphabet[cipher_index]
 
     return encoded_text
+
+
+def decrypt(cipher_text, shift_amount):
+    """decrypt cipher-text using "Caesar's Cipher" by removing the shift"""
+    cipher_length = len(alphabet)
+    decoded_text = ""
+    for char in cipher_text:
+        # handle non-alphabetical characters (e.g. spaces / punctuation)
+        if char not in alphabet:
+            decoded_text += char
+        else:
+            # calculate the cipher-decoded reference for the character
+            cipher_index = alphabet.index(char) - shift_amount
+            # keep the look-up reference within the cipher's range
+            if cipher_index > cipher_length - 1:
+                cipher_index -= cipher_length
+
+            decoded_text += alphabet[cipher_index]
+
+    return decoded_text
+
+
+# allow user to encode or decode messages
+if direction == 'encode':
+    print(encrypt(text, shift))
+elif direction == 'decode':
+    print(decrypt(text, shift))
+else:
+    raise ValueError(
+        f"Direction MUST be either 'encode' OR 'decode': {direction}")
