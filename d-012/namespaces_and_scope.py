d8276f06ccff4b6e765b4cf7acd0ae53c82bc35f
apitofme <apitofme@github.com>
2024-09-17 10:39:29 +0100

    d-012: NOTE - course content updated; lesson numbers changed (inconsistent from previous, going forward)!

---
+++ d-012/namespaces_and_scope.py
@@ -0,0 +1,14 @@
+"""
+-=< 100 Days of Python >=-
+-=[ Day 012 ]=-
+"""
+"""
+NOTE: The course was updated and reorganised, in part to move to using the
+JetBrains PyCharm IDE from the get-go. This has presumably resulted in the
+lesson numbers changing, such that previously we had reached over "L.100",
+picking up the course again (after a break over the summer) we now find
+ourselves here at...
+
+L.87 - Namespaces: Local vs Global
+
+"""
