0ecacee210bd3da1f93e1e5d88ffed523b04f61e
apitofme <apitofme@github.com>
2024-10-25 12:54:21 +0100

    d-017: Improved Quiz Game - minor tweaks to "data.py"

--- d-017/improved_quiz_game/data.py
+++ d-017/improved_quiz_game/data.py
@@ -13,41 +13,44 @@ import urllib.error
 import json
 import html
 
-# Define API options
+# Define API options:
 q_amount = 10
+q_category = 9  # <- Default to "General Knowledge" (i.e. '9')
 q_difficulty = "medium"
 q_format = "boolean"
+# NOTE: To look up category references...
+# Ref: https://opentdb.com/api_category.php
 
-# Create API query URL
+# Create API query URL:
 opentdb_api_url = "https://opentdb.com/api.php?"
 opentdb_api_url += f"amount={q_amount}"
-opentdb_api_url += "&category=9"  # General Knowledge
+opentdb_api_url += f"&category={q_category}"
 opentdb_api_url += f"&difficulty={q_difficulty}"
 opentdb_api_url += f"&type={q_format}"
 
-# Safely perform the request and get response
+# Set this to test for fallback to OG questions if request fails
+json_data = None
+# Safely perform the request and get response:
 try:
+    # Use a context handler (i.e. "with") to ensure tidy cleanup of resources
     with urllib.request.urlopen(opentdb_api_url) as response:
-        status_code = response.status
-        if status_code == 200:
+        if response.status == 200:
             try:
-                # NOTE: Unsure if better to allow this to fail if it will!
+                # NOTE: Unsure if better to allow this to fail if it will?!
                 json_data = json.loads(response.read().decode())
             except json.JSONDecodeError as e:
                 print(e)
         else:
-            print(f"HTTP Response Code: {status_code}")
-            json_data = None
+            print(f"Request Failed! HTTP Response Code: {response.status}")
 except urllib.error.HTTPError as e:
     print("HTTP Error:", e.code)
 except urllib.error.URLError as e:
     print("URL Error:", e.reason)
 
+# Build the question data list:
 if json_data is not None:
-    # print(f"Response: {json_data}")
-    # question_data = json_data['results']
     question_data = []
-    # Model the question data to match the original format
+    # Model the question data to match the original format:
     for q in json_data['results']:
         question_data.append({
             'text': html.unescape(q['question']),
