b86aeaf2aa7b31b9f9e48ee77700b1d6f69524fd
apitofme <apitofme@github.com>
2024-11-26 16:49:38 +0000

    d-019: Turtle Race challenge finished! (inc. minor enhancements)

--- d-019/turtle_challenge_race.py
+++ d-019/turtle_challenge_race.py
@@ -2,7 +2,8 @@
 -=< 100 Days of Python >=-
 -=[ Day 019 ]=-
 """
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, TK
+import random
 """
 L.143 & L.144 - Challenge: Create a Racing Turtles Minigame
 
@@ -11,6 +12,12 @@ different colour, and they should be positioned spaced out vertically along
 the left-hand edge of the screen.
 
 > Prompt the player to guess which turtle will win the race.
+
+> Have the turtles move at random increments across the screen until one has
+reached the "finish line" (i.e. the right hand edge of the screen).
+
+> Check which turtle has won the race and notify the player if they won or
+lost their bet.
 """
 window = Screen()
 window.setup(width=500, height=400)
@@ -21,11 +28,6 @@ window.setup(width=500, height=400)
 
 colours = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-bet = window.textinput(
-    title="Place your bets..",
-    prompt="Which turtle will win the race? Enter a colour: "
-)
-
 # Pre-calculate coordinates:
 x_scale = window.window_width() // 2
 y_scale = window.window_height() // 2
@@ -34,7 +36,7 @@ if divisions % 2 == 0:
     equal_dist = y_scale / (divisions // 2 + 1)
 else:
     equal_dist = y_scale / ((divisions + 1) // 2)
-print(f"Eq.D = {equal_dist}")
+# print(f"Eq.D = {equal_dist}")
 
 turtles = []
 for n, colour in enumerate(colours):
@@ -42,6 +44,8 @@ for n, colour in enumerate(colours):
     t = Turtle(shape='turtle')
     t.penup()
     t.color(colour)
+    t.name = colour
+    print(f"Turtle has been given the name: {t.name}")
     # set up the start position:
     start_x = -(x_scale) + 10  # <- Not off screen!
     start_y = -(y_scale) + (equal_dist * (n + 1))
@@ -49,9 +53,51 @@ for n, colour in enumerate(colours):
     if divisions % 2 == 0:
         # ...by adding half of the equal distance
         start_y += equal_dist / 2
-    print(start_y)
+    # print(start_y)
     t.goto(start_x, start_y)
     # store the turtle
     turtles.append(t)
+
+# Take the player's bet (with validation):
+bet = ""
+while bet.lower() not in colours:
+    bet = window.textinput(
+        title="Place your bets..",
+        prompt="Which turtle will win the race? Enter a colour: "
+    )
+
+# Set up for the race:
+finish_line = x_scale - 25  # <- Not off screen!
+winner = None
+race_over = False
+
+# Race the turtles:
+while not race_over:
+    for t in turtles:
+        x_pos = t.xcor()
+        # Calculate a move for the turtle, a random distance forward...
+        n_pos = x_pos + random.randint(1, 10)
+        # ...but prevent it from going off screen
+        if n_pos > finish_line:
+            t.setx(finish_line)
+            if winner is None:
+                winner = t.pencolor()
+        else:
+            t.setx(n_pos)
+    # Check for winner before looping
+    if winner is not None:
+        race_over = True
+
+# Check the winner:
+msg_title = "Race Results:"
+msg_text = f"The {winner.capitalize()} turtle won.\n"
+if winner == bet.lower():
+    msg_text += "Congratulations, you won!"
+else:
+    msg_text += "Sorry, you lost the bet!"
+
+# Tap in to the "TK" class from the turtle module (tkinter object)
+# to display a messagebox window on our screen:
+TK.messagebox.showinfo(title=msg_title, message=msg_text)
 
 window.exitonclick()
